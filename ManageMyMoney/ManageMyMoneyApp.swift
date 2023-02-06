fbd7b5e18e149ca9a66c858dfb46df5fceedd147
Evelyn(Yuyang) Chen <yychen@lcs.on.ca>
2023-02-05 19:21:00 -0500

    Initial Commit

---
+++ ManageMyMoney/ManageMyMoneyApp.swift
@@ -0,0 +1,17 @@
+//
+//  ManageMyMoneyApp.swift
+//  ManageMyMoney
+//
+//  Created by Evelyn Chen on 2023-02-05.
+//
+
+import SwiftUI
+
+@main
+struct ManageMyMoneyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
