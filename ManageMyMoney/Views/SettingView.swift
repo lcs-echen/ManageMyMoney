ed322eae40db8a40c6d60e821c091abf3cfd8dd1
Evelyn(Yuyang) Chen <yychen@lcs.on.ca>
2023-02-05 21:03:45 -0500

    Added a static layout for setting view

--- ManageMyMoney/Views/SettingView.swift
+++ ManageMyMoney/Views/SettingView.swift
@@ -8,13 +8,67 @@
 import SwiftUI
 
 struct SettingView: View {
+    @State var saving: Double = 5
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack (alignment: .leading){
+
+            HStack{
+                Text("Cash on hand")
+                    .font(.title)
+                Text("$")
+                TextField("Enter a numeric value", text: Binding.constant(""))
+            }
+            Text("Expenses")
+                .font(.title)
+            VStack (alignment: .leading){
+                HStack {
+                    Text("Saving              ")
+                        .font(.title3)
+                    Slider(value: $saving,
+                           in: 0...100)
+                    Text("\(saving.formatted(.number.precision(.fractionLength(1))))")
+                        .font(.title3)
+                    + Text("%")
+                        .font(.title3)
+                }
+                Text("Entertainment")
+                    .font(.title3)
+            }
+            .padding()
+            
+            HStack{
+                VStack {
+                    Text("Left")
+                        .font(.title)
+                    Text("$")
+                        .font(.title2)
+                    + Text("450")
+                        .font(.title2)
+                }
+                .padding(.leading, 30)
+                Spacer()
+                VStack {
+                    Text("Wish")
+                        .font(.title)
+                    Text("$")
+                        .font(.title2)
+                    + Text("450")
+                        .font(.title2)
+                }
+                .padding(.trailing, 30)
+                
+            }
+            Spacer()
+        }
+        .padding()
+        .navigationTitle("Manage My Money")
     }
 }
 
 struct SettingView_Previews: PreviewProvider {
     static var previews: some View {
-        SettingView()
+        NavigationView {
+            SettingView()
+        }
     }
 }
