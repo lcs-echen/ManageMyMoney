46d4d35e41dd26c5280eb73cd6ed4511a4235f11
Evelyn(Yuyang) Chen <yychen@lcs.on.ca>
2023-02-05 19:24:14 -0500

    Create several views and two groups - "views" and "model"

---
+++ ManageMyMoney/Model/WishesModel.swift
@@ -0,0 +1,8 @@
+//
+//  WishesModel.swift
+//  ManageMyMoney
+//
+//  Created by Evelyn Chen on 2023-02-05.
+//
+
+import Foundation
---
+++ ManageMyMoney/Views/WishesView.swift
@@ -0,0 +1,20 @@
+//
+//  WishesView.swift
+//  ManageMyMoney
+//
+//  Created by Evelyn Chen on 2023-02-05.
+//
+
+import SwiftUI
+
+struct WishesView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct WishesView_Previews: PreviewProvider {
+    static var previews: some View {
+        WishesView()
+    }
+}
