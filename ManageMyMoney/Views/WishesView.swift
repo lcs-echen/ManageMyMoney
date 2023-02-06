576a9381ff0a144c4e19e8f74044b5d22ffb6ad6
Evelyn(Yuyang) Chen <yychen@lcs.on.ca>
2023-02-05 20:07:59 -0500

    added a static layout

--- ManageMyMoney/Views/WishesView.swift
+++ ManageMyMoney/Views/WishesView.swift
@@ -9,12 +9,47 @@ import SwiftUI
 
 struct WishesView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        HStack {
+            VStack (alignment: .leading){
+                Group{
+                    Text("Your Wish: ")
+                        .font(.title2)
+                    TextField("Please enter your wish", text: Binding.constant(""))
+                        .padding(.bottom, 10)
+                }
+                Group {
+                    Text("Estimated cost? ")
+                        .font(.title2)
+                    TextField("Please enter the estimated cost", text: Binding.constant(""))
+                        .padding(.bottom, 10)
+                }
+                Group{
+                    Text("Amount/Times")
+                        .font(.title2)
+                    Stepper("2", value: Binding.constant(2), in: 0...20)
+                        .padding(.bottom, 15)
+                }
+
+                HStack {
+                    Text("Total Cost: ")
+                        .font(.title2)
+                    Spacer()
+                    Text("$450")
+                        .font(.title2)
+                }
+                Spacer()
+            }
+            .padding()
+            Spacer()
+        }
+        .navigationTitle("WISHES")
     }
 }
 
 struct WishesView_Previews: PreviewProvider {
     static var previews: some View {
-        WishesView()
+        NavigationView {
+            WishesView()
+        }
     }
 }
