68b3fc7a73027949570168e14185ca3a6911fb22
Evelyn(Yuyang) Chen <yychen@lcs.on.ca>
2023-04-05 12:00:52 -0400

    imported Blackbird and can now read information from the database

--- ManageMyMoney/Model/WishesModel.swift
+++ ManageMyMoney/Model/WishesModel.swift
@@ -4,19 +4,15 @@
 //
 //  Created by Evelyn Chen on 2023-02-05.
 //
-
+import Blackbird
 import Foundation
 
-struct Wishes: Identifiable {
-    let id: Int
-    let name: String
-    let totalCost: String
-    let amount: String
-    let completed: Bool
+struct Wishes: BlackbirdModel {
+    @BlackbirdColumn var id: Int
+    @BlackbirdColumn var name: String
+    @BlackbirdColumn var totalCost: String
+    @BlackbirdColumn var amount: String
+    @BlackbirdColumn var completed: Bool
 }
-let wish1 = Wishes(id: 1, name: "Hat", totalCost: "10", amount: "2", completed: false)
-let wish2 = Wishes(id: 2, name: "Sun Glasses", totalCost: "5", amount: "4", completed: false)
 
-let wishModelForPreviews = [wish1, wish2]
-    
 
