e05af17fd3511179e3c9774dedd84ccb98233d06
Evelyn(Yuyang) Chen <yychen@lcs.on.ca>
2023-04-10 13:23:26 -0400

    can now delete rows

--- ManageMyMoney/Views/CartView.swift
+++ ManageMyMoney/Views/CartView.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct CartView: View {
     @Environment(\.blackbirdDatabase) var db: Blackbird.Database?
     @Binding var totalSpending: Double
-
+    
     @BlackbirdLiveModels({ db in try await WishCart.read(from: db)}) var history
     
     var body: some View {
@@ -38,54 +38,74 @@ struct CartView: View {
                             Image(systemName: "circle")
                         }
                     })
-                .onTapGesture {
-                    Task {
-                        try await db!.transaction {
-                            core in try core.query("UPDATE TodoItem SET completed = (?) WHERE id = (?)", !history.completed, history.id)
+                    .onTapGesture {
+                        Task {
+                            try await db!.transaction {
+                                core in try core.query("UPDATE TodoItem SET completed = (?) WHERE id = (?)", !history.completed, history.id)
+                            }
                         }
                     }
                 }
-            }
-            
-            Spacer()
-            HStack {
-                Text("Total: ")
-                    .fontWeight(.semibold)
-                    .font(.title)
+                .onDelete(perform: removeRows)
+                
                 Spacer()
-                Text("$")
-                    .foregroundColor(Color("Orange"))
-                    .font(.title)
-                    .fontWeight(.semibold)
-                + Text ("\(totalSpending.formatted(.number.precision(.fractionLength(2))))")
-                    .font(.largeTitle)
-                    .fontWeight(.bold)
-                    .foregroundColor(Color("Orange"))
+                HStack {
+                    Text("Total: ")
+                        .fontWeight(.semibold)
+                        .font(.title)
+                    Spacer()
+                    Text("$")
+                        .foregroundColor(Color("Orange"))
+                        .font(.title)
+                        .fontWeight(.semibold)
+                    + Text ("\(totalSpending.formatted(.number.precision(.fractionLength(2))))")
+                        .font(.largeTitle)
+                        .fontWeight(.bold)
+                        .foregroundColor(Color("Orange"))
+                    
+                }
+                .padding(.horizontal, 30)
+                .padding(.top, 8)
+                .padding(.bottom, 25)
+                
+                
                 
             }
-            .padding(.horizontal, 30)
-            .padding(.top, 8)
-            .padding(.bottom, 25)
-            
-            
-            
-        }
-        .onAppear {
-            // Create a running total that is zero
-            var total = 0.0
-            // Iterate over each item and add to the running total
-            for item in history.results {
-                total += Double(item.totalCost) ?? 0
+            .onAppear {
+                // Create a running total that is zero
+                var total = 0.0
+                // Iterate over each item and add to the running total
+                for item in history.results {
+                    total += Double(item.totalCost) ?? 0
+                }
+                // Update the view
+                totalSpending = total
             }
-            // Update the view
-            totalSpending = total
+        }
+        
+    }
+    
+    func removeRows(at offsets: IndexSet) {
+        Task {
+            try await db!.transaction{ core in
+                var idList = ""
+                for offset in offsets {
+                    idList += "\(history.results[offset].id),"
+                }
+                print(idList)
+                idList.removeLast()
+                print(idList)
+                
+                try core.query("DELETE FROM TodoItem WHERE id IN (?)", idList)
+            }
         }
     }
 }
-
-
-struct CartView_Previews: PreviewProvider {
-    static var previews: some View {
-        CartView(totalSpending: Binding.constant(325.00))
+    
+    
+    struct CartView_Previews: PreviewProvider {
+        static var previews: some View {
+            CartView(totalSpending: Binding.constant(325.00))
+        }
     }
-}
+
