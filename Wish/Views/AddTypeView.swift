e69a463e05c86b559a152aae1d106b05bc7e82d0
Evelyn(Yuyang) Chen <yychen@lcs.on.ca>
2023-06-06 15:50:53 -0400

    Added in a AddTypeView

---
+++ Wish/Views/AddTypeView.swift
@@ -0,0 +1,62 @@
+//
+//  AddTypeView.swift
+//  Wish
+//
+//  Created by Evelyn Chen on 2023-06-06.
+//
+
+import SwiftUI
+import Blackbird
+
+struct AddTypeView: View {
+    @Environment(\.blackbirdDatabase) var db: Blackbird.Database?
+    
+    @State var type: String = ""
+    var body: some View {
+        NavigationView{
+
+            HStack {
+                Text("Type: ")
+                    .font(.title2)
+                TextField("Please enter the type you want to add", text: $type)
+                    .padding(.bottom, 10)
+            }
+            .toolbar {
+                            ToolbarItem(placement: .primaryAction) {
+                                Button(action: {
+                                    addWish()
+                                }, label: {
+                                    Text("Add")
+                                })
+                                .disabled(type == "")
+                            }
+                        }
+
+        }
+       
+    }
+    
+    func addWish() {
+        Task {
+            try await db!.transaction { core in
+                try core.query("""
+                            INSERT INTO WishType (
+                            type
+                            )
+                            VALUES (
+                                (?)
+                            )
+                            """,
+                            type)
+            }
+            // Reset input fields after writing to database
+            type = ""
+        }
+    }
+}
+
+struct AddTypeView_Previews: PreviewProvider {
+    static var previews: some View {
+        AddTypeView()
+    }
+}
